ef08f62a8f12ab1bddb7263572f68f1c7ff6025f
Denis Stępień <156847651+raiczuu@users.noreply.github.com>
2024-02-14 16:30:02 +0100

    Update wyszukiwania.cpp

--- wyszukiwania.cpp
+++ wyszukiwania.cpp
@@ -3,8 +3,8 @@
 using namespace std;
 
 /////////////////  Algorytm wyszukiwania liniowego
-int liniowe(int *t, int size, int key) {
-    for (int i = 0; i < size; ++i) {
+int liniowe(int *t, int size, int key) { // Przechodzimy przez wszystkie elementy tablicy
+    for (int i = 0; i < size; ++i) { // Jeśli znaleźliśmy klucz, zwracamy jego indeks
         if (t[i] == key) {
             return i; // zwraca indeks, jezeli element byl znaleziony
         }
