5d78f9d156a30782d1069b075732ecc4d3b65591
Denis Stępień <156847651+raiczuu@users.noreply.github.com>
2024-02-14 16:28:48 +0100

    Update wyszukiwania.cpp

--- wyszukiwania.cpp
+++ wyszukiwania.cpp
@@ -51,27 +51,3 @@ int binarne(int *t, int size, int key) {
 
     return -1; // Zwraca -1, jezeli nie byl
 }
-
-int main() {
-    // Przykladowe uzycie sortowania wyszukiwania
-    int tablica[] = {10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120};
-    int rozmiar = sizeof(tablica) / sizeof(tablica[0]);
-
-    // liniowe
-    int klucz1 = 40;
-    int indeks1 = liniowe(tablica, rozmiar, klucz1);
-    cout << "Liniowe wyszukiwanie: " << indeks1 << endl;
-
-    // Liniowe z wartownikem
-    int klucz2 = 50;
-    int indeks2 = wartownikiem(tablica, rozmiar, klucz2);
-    cout << "Liniowe wyszukiwanie z wartownikiem: " << indeks2 << endl;
-
-    // skokowe
-    
-    // binarne
-    int klucz4 = 20;
-    int indeks4 = binarne(tablica, rozmiar, klucz4);
-    cout << "Binarne wyszukiwanie: " << indeks4 << endl;
-    return 0;
-}
