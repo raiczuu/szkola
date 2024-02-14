e0fe28958c77d2b606d08ee76bcededbb70b52f9
Denis Stępień <156847651+raiczuu@users.noreply.github.com>
2024-02-14 03:04:08 +0100

    sortowanie.cpp

--- sortowanie.cpp
+++ sortowanie.cpp
@@ -2,8 +2,8 @@
 using namespace std;
 
 /////////////////////// Algorytm sortowania bąbelkowego
-void sortowanieBabelkowe(int tab[], int rozmiar) {
-    for (int i = 0; i < rozmiar - 1; ++i) {
+void sortowanieBabelkowe(int tab[], int rozmiar) {        
+    for (int i = 0; i < rozmiar - 1; ++i) {                 
         for (int j = 0; j < rozmiar - i - 1; ++j) {
             if (tab[j] > tab[j + 1]) {
                 // Zamień miejscami elementy, jeżeli są w złej kolejności
@@ -25,18 +25,19 @@ void sortowanieKoktajlowe(int tab[], int rozmiar) {
         // Przechodzenie od lewej do prawej strony
         for (int i = poczatek; i < koniec; ++i) {
             if (tab[i] > tab[i + 1]) {
-                swap(tab[i], tab[i + 1]);
+                swap(tab[i], tab[i + 1]);   // zamienia miejscami 2 elementy
                 zamienione = true;
             }
         }
 
-        // Jeżeli nie było zamiany, to zakończ pętlę
-        if (!zamienione) {
+        // jesli nie bylo zmiany, zakoncz petle
+        if (!zamienone) {
             break;
         }
 
-        // Zmniejsz zakres o 1, bo ostatni element jest już na swoim miejscu
-        --koniec;
+        // zmniejsz zakres o 1
+
+        --koniec; // operator dekrementacji, zmiejsza wartosc zmiennej koniec o 1
 
         // Przechodzenie od prawej do lewej strony
         for (int i = koniec - 1; i >= poczatek; --i) {
@@ -46,70 +47,71 @@ void sortowanieKoktajlowe(int tab[], int rozmiar) {
             }
         }
 
-        // Zwiększ zakres o 1, bo pierwszy element jest już na swoim miejscu
-        ++poczatek;
+        // zwieksz zakres o 1
+
+        ++poczatek; // operator inkrementacji, zwieksza wartosc zmiennej o 1
     }
 }
 
 /////////////////////// Algorytm sortowania przez scalanie
-void scalanie(int tab[], int lewy, int srodek, int prawy) {
-    int a = srodek - lewy + 1;
-    int b = prawy - srodek;
+void scalanie(int tab[], int lewa, int srodek, int prawa) {
+    int a = srodek - lewa + 1;
+    int b = prawa - srodek;
 
-    int *lewaTablica = new int[a];
+    int *lewaTablica = new int[a];   // sluzy do przydzielenia pamieci dla tablicy liczb. Calk, zwracania wskaznika do pierwszego elementu tablicy
     int *prawaTablica = new int[b];
 
-    // Skopiuj dane do tymczasowych tablic
+    // kopiowanie danych do tymczasowej tablicy
     for (int i = 0; i < a; ++i) {
-        lewaTablica[i] = tab[lewy + i];
+        lewaTablica[i] = tab[lewa + i];
     }
     for (int j = 0; j < b; ++j) {
         prawaTablica[j] = tab[srodek + 1 + j];
     }
 
-    // Scalanie dwóch tymczasowych tablic w jedną posortowaną
+    // laczenie dwoch tymczasowych tablic w 1 posortowana
     int i = 0;
     int j = 0;
-    int k = lewy;
-    while (i < a && j < b) {
+    int k = lewa;
+    while (i < a && j < b) {       // petla bedzie kontynuowana dopoki oba warunki sa spelnione
         if (lewaTablica[i] <= prawaTablica[j]) {
             tab[k] = lewaTablica[i];
-            ++i;
+            ++i; // inkrementacja i o + 1
         } else {
             tab[k] = prawaTablica[j];
-            ++j;
+            ++j; // inkrementacja j o + 1
         }
-        ++k;
+        ++k; // inkrementacja k o + 1
     }
 
-    // Skopiuj pozostałe elementy z lewej tablicy (jeżeli są)
+    // jezeli sa pozostale elementy lewej tablicy, skopiuj je
     while (i < a) {
-        tab[k] = lewaTablica[i];
-        ++i;
-        ++k;
+        tab[k] = lewaTablica[i]; // przepisuje wartosci z lewej tablicy do innej
+        ++i; // inkrementacja i o + 1
+        ++k; // inkrementacja k o + 1
     }
 
-    // Skopiuj pozostałe elementy z prawej tablicy (jeżeli są)
+    // jezeli sa pozostale elementy prawej tablicy, skopiuj je
     while (j < b ) {
-        tab[k] = prawaTablica[j];
-        ++j;
-        ++k;
+        tab[k] = prawaTablica[j]; // przepisuje wartosci z prawej tablicy do innej
+        ++j; // inkrementacja j o + 1
+        ++k; // inkrementacja k o + 1
     }
 
-    delete[] lewaTablica;
-    delete[] prawaTablica;
+    delete[] lewaTablica;  // zwalnia pamiec ktora wczesciej przydzielilismy
+    delete[] prawaTablica; // zwalnia pamiec ktora wczesciej przydzielilismy
 }
 
-void sortowaniePrzezScalanie(int tab[], int lewy, int prawy) {
-    if (lewy < prawy) {
-        int srodek = lewy + (prawy - lewy) / 2;
+void sortowaniePrzezScalanie(int tab[], int lewa, int prawa) {
+    if (lewa < prawa) {
+        int srodek = lewa + (prawa - lewa) / 2;  // oblicza srodek przedzialu miedzy lewa a prawa
 
-        // Sortuj lewą i prawą część
-        sortowaniePrzezScalanie(tab, lewy, srodek);
-        sortowaniePrzezScalanie(tab, srodek + 1, prawy);
+        // Sort lewej i prawej czesci
+        sortowaniePrzezScalanie(tab, lewa, srodek);  // posortowanie tej czesci
+        sortowaniePrzezScalanie(tab, srodek + 1, prawa); // posortowanie tej czesci 
 
-        // Scal posortowane części
-        scalanie(tab, lewy, srodek, prawy);
+        // Scal posortowane czesci
+        scalanie(tab, lewa, srodek, prawa);  // laczy 2 posortowane fragmenty tablicy
     }
 }
 
@@ -119,28 +121,28 @@ int szybkie(int tab[], int niski, int wysoki) {
     int i = niski - 1;        // Indeks mniejszego elementu
 
     for (int j = niski; j <= wysoki - 1; ++j) {
-        if (tab[j] < pivot) {
-            ++i;
+        if (tab[j] < pivot) {  // sprawdza czy wartosc j w tab jest mniejsza od pivota
+            ++i; // inkrementacja i o 1
             swap(tab[i], tab[j]);
         }
     }
 
-    swap(tab[i + 1], tab[wysoki]);
+    swap(tab[i + 1], tab[wysoki]); // zamienia miejcami wartosci dwoch elemtnow w tab
     return i + 1;
 }
 
 void sortowanieSzybkie(int tab[], int niski, int wysoki) {
     if (niski < wysoki) {
         // Znajdź indeks podziału
-        int pi = szybkie(tab, niski, wysoki);
+        int pi = szybkie(tab, niski, wysoki);  // pi = pivot, wywoluje funkcje
 
         // Sortuj osobno elementy przed i po podziale
-        sortowanieSzybkie(tab, niski, pi - 1);
-        sortowanieSzybkie(tab, pi + 1, wysoki);
+        sortowanieSzybkie(tab, niski, pi - 1); // To wywolajanie odnosi sie do lewej czesci tablicy gdzie elemtny sa mniejsze niz pivot
+        sortowanieSzybkie(tab, pi + 1, wysoki); // --- | --- do prawej czesci 
     }
 }
 
-// Algorytm sortowania przez  Wstawianie
+/////////////////////// Algorytm sortowania przez  Wstawianie
 void sortowaniePrzezWstawianie(int tab[], int rozmiar) {
     for (int i = 1; i < rozmiar; ++i) {
         int klucz = tab[i];
@@ -148,9 +150,9 @@ void sortowaniePrzezWstawianie(int tab[], int rozmiar) {
 
         // Przesuwanie elementów większych niż klucz
         // do jednego miejsca w prawo
-        while (j >= 0 && tab[j] > klucz) {
+        while (j >= 0 && tab[j] > klucz) { 
             tab[j + 1] = tab[j];
-            --j;
+            --j; // dekramentacja j o - 1
         }
 
         tab[j + 1] = klucz;
@@ -160,8 +162,8 @@ void sortowaniePrzezWstawianie(int tab[], int rozmiar) {
 int main() {
     // Przykładowe użycie sortowania bąbelkowego
     int tabBabelkowe[] = {64, 34, 25, 12, 22, 11, 90};
-    int rozmiarBabelkowe = sizeof(tabBabelkowe) / sizeof(tabBabelkowe[0]);
-    sortowanieBabelkowe(tabBabelkowe, rozmiarBabelkowe);
+    int rozmiarBabelkowe = sizeof(tabBabelkowe) / sizeof(tabBabelkowe[0]); // oblicza rozmiar tablicy
+    sortowanieBabelkowe(tabBabelkowe, rozmiarBabelkowe); // przekazuje tablice i jej rozmiar jako argument
 
     cout << "Sortowanie bąbelkowe:\n";
     for (int i = 0; i < rozmiarBabelkowe; ++i) {
@@ -171,7 +173,7 @@ int main() {
 
     // Przykładowe użycie sortowania koktajlowego
         int tabKoktajlowego[] = {64, 34, 25, 12, 22, 11, 90};
-    int rozmiarKoktajlowego = sizeof(tabKoktajlowego) / sizeof(tabKoktajlowego[0]);
+    int rozmiarKoktajlowego = sizeof(tabKoktajlowego) / sizeof(tabKoktajlowego[0]); 
     sortowanieKoktajlowe(tabKoktajlowego, rozmiarKoktajlowego);
 
     cout << "Sortowanie koktajlowe:\n";
