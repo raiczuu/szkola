06558308f56bf92c0eed90cf723c058d01982e05
Denis Stępień <156847651+raiczuu@users.noreply.github.com>
2024-02-14 11:06:58 +0100

    sortowanie.cpp

--- sortowanie.cpp
+++ sortowanie.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include "sortowanie.hpp"
 using namespace std;
 
 /////////////////////// Algorytm sortowania bąbelkowego
@@ -157,63 +158,4 @@ void sortowaniePrzezWstawianie(int tab[], int rozmiar) {
 
         tab[j + 1] = klucz;
     }
-}
-
-int main() {
-    // Przykladowe uzycie sortowania babelkowego
-    int tabBabelkowe[] = {64, 34, 25, 12, 22, 11, 90};
-    int rozmiarBabelkowe = sizeof(tabBabelkowe) / sizeof(tabBabelkowe[0]); // oblicza rozmiar tablicy
-    sortowanieBabelkowe(tabBabelkowe, rozmiarBabelkowe); // przekazuje tablice i jej rozmiar jako argument
-
-    cout << "Sortowanie babelkowe:\n";
-    for (int i = 0; i < rozmiarBabelkowe; ++i) {
-        cout << tabBabelkowe[i] << " ";
-    }
-    cout << "\n\n";
-
-    // koktajlowego
-        int tabKoktajlowego[] = {64, 34, 25, 12, 22, 11, 90};
-    int rozmiarKoktajlowego = sizeof(tabKoktajlowego) / sizeof(tabKoktajlowego[0]); 
-    sortowanieKoktajlowe(tabKoktajlowego, rozmiarKoktajlowego);
-
-    cout << "Sortowanie koktajlowe:\n";
-    for (int i = 0; i < rozmiarKoktajlowego; ++i) {
-        cout << tabKoktajlowego[i] << " ";
-    }
-    cout << "\n\n";
-
-    // scalanie
-    int tabScalanie[] = {64, 34, 25, 12, 22, 11, 90};
-    int rozmiarScalanie = sizeof(tabScalanie) / sizeof(tabScalanie[0]);
-    sortowaniePrzezScalanie(tabScalanie, 0, rozmiarScalanie - 1);
-
-    cout << "Sortowanie przez scalanie:\n";
-    for (int i = 0; i < rozmiarScalanie; ++i) {
-        cout << tabScalanie[i] << " ";
-    }
-    cout << "\n\n";
-
-    // szybkiego
-    int tabSzybkie[] = {64, 34, 25, 12, 22, 11, 90};
-    int rozmiarSzybkie = sizeof(tabSzybkie) / sizeof(tabSzybkie[0]);
-    sortowanieSzybkie(tabSzybkie, 0, rozmiarSzybkie - 1);
-
-    cout << "Sortowanie szybkie:\n";
-    for (int i = 0; i < rozmiarSzybkie; ++i) {
-        cout << tabSzybkie[i] << " ";
-    }
-    cout << "\n\n";
- 
-    // wstawianie
-    int tabWstawianie[] = {64, 34, 25, 12, 22, 11, 90};
-    int rozmiarWstawianie = sizeof(tabWstawianie) / sizeof(tabWstawianie[0]);
-    sortowaniePrzezWstawianie(tabWstawianie, rozmiarWstawianie);
-
-    cout << "Sortowanie przez wstawianie:\n";
-    for (int i = 0; i < rozmiarWstawianie; ++i) {
-        cout << tabWstawianie[i] << " ";
-    }
-    cout << "\n\n";
- 
-    return 0;
 }
