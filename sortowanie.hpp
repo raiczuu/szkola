d35708d253a1a1dbe14369afcac7d0830f82546f
Denis Stępień <156847651+raiczuu@users.noreply.github.com>
2024-02-14 11:11:26 +0100

    sortowanie.hpp

--- sortowanie.hpp
+++ sortowanie.hpp
@@ -5,7 +5,7 @@ void sortowanieKoktajlowe(int tab[], int rozmiar); // sortowanie koktajlowe
 void scalanie(int tab[], int lewa, int srodek, int prawa); // sortowanie scalanie
 void sortowaniePrzezScalanie(int tab[], int lewa, int prawa);
 
-int szybkie(int tab[], int niski, int wysoki) // sortowanie szybkie
-void sortowanieSzybkie(int tab[], int niski, int wysoki)
+int szybkie(int tab[], int niski, int wysoki); // sortowanie szybkie
+void sortowanieSzybkie(int tab[], int niski, int wysoki);
 
-void sortowaniePrzezWstawianie(int tab[], int rozmiar) // sortowanie wstawianie
+void sortowaniePrzezWstawianie(int tab[], int rozmiar); // sortowanie wstawianie
