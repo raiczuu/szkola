79a5ddfe351ad1b5b224dee334fa791ab3dae53c
Denis Stępień <156847651+raiczuu@users.noreply.github.com>
2024-02-14 16:28:17 +0100

    Update wyszukiwania.hpp

--- wyszukiwania.hpp
+++ wyszukiwania.hpp
@@ -2,6 +2,4 @@ int liniowe(int *t, int size, int key); // wyszukiwanie liniowe
 
 int wartownikiem(int *t, int size, int key); // wyszukiwania liniowego z wartownikiem
 
-int skokowe(int *t, int size, int key); // wyszukiwania skokowego
-
 int binarne(int *t, int size, int key); // wyszukiwania binarnego
