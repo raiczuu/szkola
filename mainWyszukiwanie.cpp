8302068639251cfc4c112f3ef3383eacb2dd33a0
Denis Stępień <156847651+raiczuu@users.noreply.github.com>
2024-02-14 11:10:02 +0100

    mainWyszukiwanie.cpp

---
+++ mainWyszukiwanie.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include "wyszukiwanie.hpp"
+using namespace std;
+
+int main() {
+    // Przykladowe uzycie sortowania wyszukiwania
+    int tablica[] = {10, 20, 30, 40, 50, 60, 70};
+    int rozmiar = sizeof(tablica) / sizeof(tablica[0]);
+
+    // liniowe
+    int klucz1 = 40;
+    int indeks1 = liniowe(tablica, rozmiar, klucz1);
+    cout << "Liniowe wyszukiwanie: " << indeks1 << endl;
+
+    // Liniowe z wartownikem
+    int klucz2 = 50;
+    int indeks2 = wartownikiem(tablica, rozmiar, klucz2);
+    cout << "Liniowe wyszukiwanie z wartownikiem: " << indeks2 << endl;
+
+    // skokowe
+    int klucz3 = 60;
+    int indeks3 = skokowe(tablica, rozmiar, klucz3);
+    cout << "Skokowe wyszukiwanie: " << indeks3 << endl;
+
+    // binarne
+    int klucz4 = 20;
+    int indeks4 = binarne(tablica, rozmiar, klucz4);
+    cout << "Binarne wyszukiwanie: " << indeks4 << endl;
+    return 0;
+}
